0afe2772a78c738f61f19f66019264863eda5d3c
MushroomGecko <70172740+MushroomGecko@users.noreply.github.com>
2022-07-12 22:33:33 -0400

    Added /tell, /msg, and /w support
    Added support for the /tell, /msg, and /w commands to be rerouted through the server announcements

--- src/main/java/me/mushroomgecko/reroutechat/RerouteChat.java
+++ src/main/java/me/mushroomgecko/reroutechat/RerouteChat.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RerouteChat extends JavaPlugin implements Listener {
@@ -29,11 +30,59 @@ public final class RerouteChat extends JavaPlugin implements Listener {
         String message = playerMessage.getMessage();
 
         // Sets up the output of the message originally being sent
-        String output = "[" + player.getName() + "]: " + message;
+        String output = "<" + player.getName() + "> " + message;
 
         // Reroutes the player's original message to be said by the server instead of the player
         Bukkit.broadcastMessage(output);
     }
+
+    @EventHandler
+    public void onTell(PlayerCommandPreprocessEvent playerMessage)
+    {
+        // Get the command the player sends
+        String message = playerMessage.getMessage();
+        // Split up the command
+        String[] breakMessage = message.split(" ");
+
+        // Check if a valid message is being sent
+        if(breakMessage.length > 2)
+        {
+            // Check if it is the tell command
+            if(breakMessage[0].equalsIgnoreCase("/tell") || breakMessage[0].equalsIgnoreCase("/msg") || breakMessage[0].equalsIgnoreCase("/w"))
+            {
+                // Cancels the player's original message being sent
+                playerMessage.setCancelled(true);
+
+                // Gets info about the sender
+                Player sender = playerMessage.getPlayer();
+
+                // Gets info about the receiver and gets the Player object of the receiver
+                String receiver = breakMessage[1];
+                Player recipient = getServer().getPlayerExact(receiver);
+
+                // Make a substring of the message to send to the receiver
+                String sendMessage = message.substring(message.indexOf(" ")+1 + message.substring(message.indexOf(" ")+1).indexOf(" ")+1);
+
+                // Check to see if the recipient exists
+                if(recipient != null)
+                {
+                    // Sets up the output of the message originally being sent
+                    String receiverOutput = sender.getName() + " whispers to you: " + sendMessage;
+                    String senderOutput = "You whisper to " + receiver + ": " + sendMessage;
+
+                    // Reroutes the player's original message to be said by the server instead of the player
+                    recipient.sendMessage(receiverOutput);
+                    sender.sendMessage(senderOutput);
+                }
+                else
+                {
+                    // Send an error message if the receiver is not found
+                    sender.sendMessage("Player " + receiver + " not found.");
+                }
+            }
+        }
+    }
+
 
     @Override
     public void onDisable() {
